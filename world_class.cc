1a556b572af5d011265b3f5e301f11e97fd82cef
tayona_1057178 <1057178@my.scccd.edu>
2026-02-14 17:05:48 -0800

    Defined world class cc and h

--- world_class.cc
+++ world_class.cc
@@ -1,7 +1,50 @@
 #include "/public/read.h" // IWYU pragma: keep
 #include <vector>         // IWYU pragma: keep
+#include "world_class.h"
 using namespace std;
 
-int main() {
-	cout << "Hello World!" << endl;
+World::World() {
+	size_row = 0;
+	size_col = 0;
+	map_row = 0;
+	map_col = 0;
+}
+
+World::World(int sRow, int sCol, char mRow, char mCol) {
+	size_row = sRow;
+	size_col = sCol;
+	map_row = mRow;
+	map_col = mCol;
+}
+
+void World::setSize_row(int sRow) {
+	size_row = sRow;
+}
+
+void World::setSize_col(int sCol) {
+   size_col = sCol;
+}
+
+void World::setMap_row(char mRow) {
+	map_row = mRow;
+}
+
+void World::setMap_col(char mCol) {
+	map_col = mCol;
+}
+
+int World::getSize_row() const {
+	return size_row;
+}
+
+int World::getSize_col() const {
+	return size_col;
+}
+
+int World::getMap_row() const {
+	return map_row;
+}
+
+int World::getMap_col() const {
+	return map_col;
 }
--- world_class.h
+++ world_class.h
@@ -1,7 +1,36 @@
+#pragma once
+#include <iostream>
 #include "/public/read.h" // IWYU pragma: keep
 #include <vector>         // IWYU pragma: keep
 using namespace std;
 
-int main() {
-	cout << "Hello World!" << endl;
-}
+class World {
+	private:
+		int size_row;
+		int size_col;
+		char map_row;
+		char map_col;
+	public:
+		World();
+		World(int sRow, int sCol, char mRow, char mCol);
+		//Getters and setters
+		void setSize_row(int sRow);
+		void setSize_col(int sCol);
+		void setMap_row(char mRow);
+		void setMap_col(char mCol);
+
+		int getSize_row() const;
+		int getSize_col() const;
+		int getMap_row() const;
+		int getMap_col() const;
+
+		//Methods
+
+		int* at(int row, int col);
+		int physics(); //NOTE: Change this, this is a placeholder
+		int size();
+		int alive_count();
+		void save();
+		void load();
+
+};
