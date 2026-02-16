69e22b2897cabdbb8510568b8fe51f09e10cb078
tayona_1057178 <1057178@my.scccd.edu>
2026-02-15 21:03:28 -0800

    defined functions in world_class files

--- world_class.h
+++ world_class.h
@@ -8,24 +8,32 @@ class World {
 	private:
 		int size_row;
 		int size_col;
-		char map_row;
-		char map_col;
+		vector<vector<char>> worldMap;
+		//char map_row;
+		//char map_col;
 	public:
 		World();
 		World(int sRow, int sCol, char mRow, char mCol);
+		//vector<vector<char>> worldMap;
 		//Getters and setters
 		void setSize_row(int sRow);
 		void setSize_col(int sCol);
-		void setMap_row(char mRow);
-		void setMap_col(char mCol);
-
+		//void setMap_row(char mRow);
+		//void setMap_col(char mCol);
 		int getSize_row() const;
 		int getSize_col() const;
-		int getMap_row() const;
-		int getMap_col() const;
+		//int getMap_row() const;
+		//int getMap_col() const;
+
+		char getLocation(vector<vector<char>>& map, size_t row, size_t col);
+		
+		void setLocation(vector<vector<char>>& map, size_t row, size_t col, char c);
+
+		void printMap(vector<vector<char>>& map, size_t row, size_t col);
 
 		//Methods
-
+		
+		
 		int* at(int row, int col);
 		int physics(); //NOTE: Change this, this is a placeholder
 		int size();
